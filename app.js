b15fed6c2ae4637298a957af811cfc47427b10e6
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-11-28 20:14:27 +0900

    meditateAnimationの関数を作成する

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const text = document.getElementById(".text");
 let totalTime = 7500;
 let breatheTime = (totalTime / 5) * 2;
 let holdTime = totalTime / 5;
+
+function meditateAnimation() {
+    
+}
